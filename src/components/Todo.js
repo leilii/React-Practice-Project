f6f873eb0d8d3cd5076f1fe096eca8a16ce44884
leilii <leilanajafi76@gmail.com>
2021-06-19 19:11:48 +0100

    Props&dynamic content

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+function Todo(props) {
+    return (
+        <div className='card'>
+            <h2>{props.text}</h2>
+            <div className='action'>
+              <button className='btn'>Delete</button>
+            </div> 
+        </div>
+   
+
+
+    );
+}
+
+export default Todo;
